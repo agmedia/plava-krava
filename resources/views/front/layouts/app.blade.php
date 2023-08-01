70cd64198cc61a2ffb9ec9a9c93ad831e4716031
tomashina <tomislav@agmedia.hr>
2023-08-01 14:12:04 +0200

    ffix search zoom

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="Plava Krava">
     @stack('meta_tags')
     <!-- Viewport-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <!-- Favicon and Touch Icons-->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ config('settings.images_domain') . 'favicon-32x32.png' }}">
