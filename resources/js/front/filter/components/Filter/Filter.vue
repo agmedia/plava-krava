4ca9d47b934637e4d923753ab385b77b41422f3b
tomashina <tomislav@agmedia.hr>
2023-08-02 08:21:22 +0200

    fix publisher

--- resources/js/front/filter/components/Filter/Filter.vue
+++ resources/js/front/filter/components/Filter/Filter.vue
@@ -4,13 +4,7 @@
                 <!-- Categories-->
                 <div class="widget widget-categories" v-if="categories">
 
-
-
-
-
                     <div class="accordion " id="shop-categories">
-
-
 
                         <div class="accordion-item border-bottom" v-for="cat in categories">
                             <h3 class="accordion-header px-grid-gutter" v-if="category && (category.id == cat.id)" >
@@ -71,7 +65,19 @@
 
                         </div>
                     </div>
+                    <div class="accordion-item border-bottom">
+                        <h3 class="accordion-header px-grid-gutter"  >
+                            <!--type="button"  -->
+                            <a :href="origin + 'autor'" class="nav-link-style d-block  fs-md fw-normal py-3" role="link">
+                                <span class="d-flex align-items-center"><i class="ci-add-user fs-lg mt-n1 me-2"></i> Autori </span>
+                            </a>
+                        </h3>
+                    </div>
 
+                    <div class="accordion-item border-bottom">
+                        <h3 class="accordion-header px-grid-gutter">
+                            <a class="nav-link-style d-block fs-md fw-normal py-3" :href="origin + 'nakladnik'"><span class="d-flex align-items-center"><i class="ci-edit  fs-lg mt-n1 me-2"></i>Izdavači</span></a></h3>
+                    </div>
 
 
                 </div>
@@ -354,7 +360,7 @@
              *
              */
             closeWindow() {
-                //$('#shop-sidebar').removeClass('collapse show');
+                $('#shop-sidebar').removeClass('collapse show');
             }
         }
     };
