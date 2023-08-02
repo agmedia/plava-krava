117ee835f738ce256874957cff387d01982cb1a8
tomashina <tomislav@agmedia.hr>
2023-08-02 10:18:33 +0200

    strech list

--- resources/views/front/layouts/widget/widget_product_carousel.blade.php
+++ resources/views/front/layouts/widget/widget_product_carousel.blade.php
@@ -9,8 +9,8 @@
         @endif
 
     </div>
-    <div class="tns-carousel tns-controls-static tns-controls-outside tns-nav-enabled pt-2">
-        <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 1, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
+    <div class="tns-carousel pt-4 pb-2">
+        <div class="tns-carousel-inner" data-carousel-options='{"items": 2, "controls": true, "nav": true, "autoHeight": true, "responsive": {"0":{"items":2, "gutter": 10},"500":{"items":2, "gutter": 18},"768":{"items":3, "gutter": 20}, "1100":{"items":4, "gutter": 30}, "1500":{"items":5, "gutter": 30}}}'>
             @foreach ($data['items'] as $product)
                 <!-- Product-->
                     <div>
