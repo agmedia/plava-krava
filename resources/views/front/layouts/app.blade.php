8726b9f8af9cf1282f52666ca0633cb2cfd82374
tomashina <tomislav@agmedia.hr>
2023-08-04 10:01:19 +0200

    front login

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -215,6 +215,17 @@
 
 <script src="{{ asset('js/theme.min.js') }}"></script>
 
+<script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}"></script>
+<script>
+    grecaptcha.ready(function() {
+        grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'register'}).then(function(token) {
+            if (token) {
+                document.getElementById('recaptcha').value = token;
+            }
+        });
+    });
+</script>
+
 <script>
     $(() => {
         $('#search-input').on('keyup', (e) => {
@@ -258,9 +269,7 @@
 </script>
 -->
 @stack('js_after')
-@push('js_after')
-    @include('front.layouts.partials.recaptcha-js')
-@endpush
+
 
 </body>
 </html>
