94c35681d51cae0b22bd6b6930cfe1d63ccd4f4e
tomashina <tomislav@agmedia.hr>
2023-08-07 13:45:30 +0200

    foter and category slider

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -18,16 +18,16 @@
     <div class="wave-container"></div>
     <footer class="footer bg-light pt-5">
         <div class="container-fluid pt-lg-1 px-lg-5 pt-2 pb-1 ">
-            <div class="row pt-lg-2 text-left px-3">
-                <div class="col-lg-3 col-sm-6 col-12 mb-grid-gutter">
+            <div class="row pt-lg-2 text-left px-3 px-sm-1">
+                <div class="col-lg-3 col-sm-6 col-6 mb-grid-gutter">
 
-                    <div class="d-inline-flex align-items-center text-start"><i class="ci-truck text-primary" style="font-size: 3rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Brza dostava</p> <p class="text-dark fs-ms opacity-70 mb-0">Unutar 3 radna dana</p></div></div></div>
+                    <div class="d-inline-flex align-items-top-center text-start"><i class="ci-truck text-primary" style="font-size: 2.6rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Brza dostava</p> <p class="text-dark fs-ms opacity-70 mb-0">Unutar 3 radna dana</p></div></div></div>
 
-                <div class="col-lg-3 col-sm-6 col-12 mb-grid-gutter"><div class="d-inline-flex align-items-center text-start"><i class="ci-security-check text-primary" style="font-size: 3rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Sigurna kupovina</p> <p class="text-dark fs-ms opacity-70 mb-0">SSL certifikat i CorvusPay</p></div></div></div>
+                <div class="col-lg-3 col-sm-6 col-6 mb-grid-gutter"><div class="d-inline-flex align-items-top-center text-start"><i class="ci-security-check text-primary" style="font-size: 2.6rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Sigurna kupovina</p> <p class="text-dark fs-ms opacity-70 mb-0">SSL certifikat i CorvusPay</p></div></div></div>
 
-                <div class="col-lg-3 col-sm-6 col-12 mb-grid-gutter"><div class="d-inline-flex align-items-center text-start"><i class="ci-bag text-primary" style="font-size: 3rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Besplatna dostava</p> <p class="text-dark fs-ms opacity-70 mb-0">Za narudžbe iznad 30 €</p></div></div></div>
+                <div class="col-lg-3 col-sm-6 col-6 mb-grid-gutter"><div class="d-inline-flex align-items-top-center text-start"><i class="ci-bag text-primary" style="font-size: 2.6rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Besplatna dostava</p> <p class="text-dark fs-ms opacity-70 mb-0">Za narudžbe iznad 30 €</p></div></div></div>
 
-                <div class="col-lg-3 col-sm-6 col-12 mb-grid-gutter"><div class="d-inline-flex align-items-center text-start"><i class="ci-locked text-primary" style="font-size: 3rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Zaštita kupca</p> <p class="text-dark fs-ms opacity-70 mb-0">Od narudžbe pa sve do dostave</p></div></div></div>
+                <div class="col-lg-3 col-sm-6 col-6 mb-grid-gutter"><div class="d-inline-flex align-items-top-center text-start"><i class="ci-locked text-primary" style="font-size: 2.6rem;"></i> <div class="ps-3"><p class="text-dark fw-bold fs-base mb-1">Zaštita kupca</p> <p class="text-dark fs-ms opacity-70 mb-0">Od narudžbe pa sve do dostave</p></div></div></div>
 
             </div>
         </div>
