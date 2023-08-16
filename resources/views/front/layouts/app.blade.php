36e670998757183b00c68477b1e5af6eb90dd8cb
tomashina <tomislav@agmedia.hr>
2023-08-16 08:45:23 +0200

    load fix

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -171,7 +171,7 @@
 
 <script src="{{ asset('js/theme.min.js') }}"></script>
 
-<script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}" async defer></script>
+<script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.sitekey') }}"></script>
 <script>
     grecaptcha.ready(function() {
         grecaptcha.execute('{{ config('services.recaptcha.sitekey') }}', {action: 'register'}).then(function(token) {
