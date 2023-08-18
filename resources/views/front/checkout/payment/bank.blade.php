41c6ddee2b34182f32ed0b26211db70c81ef2459
tomashina <tomislav@agmedia.hr>
2023-08-18 10:01:08 +0200

    fast click

--- resources/views/front/checkout/payment/bank.blade.php
+++ resources/views/front/checkout/payment/bank.blade.php
@@ -6,7 +6,7 @@
         <a class="btn btn-secondary d-block w-100" href="{{ route('naplata') }}"><i class="ci-arrow-left  me-1"></i><span class="d-none d-sm-inline">Povratak na plaćanje</span><span class="d-inline d-sm-none">Povratak</span></a>
     </div>
     <div class="w-50 ps-2">
-        <button style="z-index:10000" class="btn btn-primary d-block w-100" type="submit"><span>Završi kupnju</span><i class="ci-arrow-right  ms-1"></i></button>
+        <button style="z-index:10000" class="btn btn-primary w-100" type="submit"><span>Završi kupnju</span><i class="ci-arrow-right  ms-1"></i></button>
     </div>
 
     </div>
