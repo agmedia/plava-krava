547fd006302aa9d7ec39b5953e15d8644076ecd8
tomashina <tomislav@agmedia.hr>
2023-08-21 08:46:47 +0200

    mail fix

--- resources/views/emails/layouts/partials/order-price-table.blade.php
+++ resources/views/emails/layouts/partials/order-price-table.blade.php
@@ -56,9 +56,9 @@
             <td style="border-left: none; border-right: none; border-top: none;"></td>
             <td style="border-left: none; text-align: right; border-top: none; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}">{{ $total->title }}</td>
             @if ($order->shipping_state != 'Croatia' && $total->code == 'shipping')
-                <td style="border-left: none; text-align: right; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">Trošak dostave će Vam biti poslan u roku od 24h.</td>
+                <td style="border-left: none; text-align: right; border-top: none; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">Trošak dostave će Vam biti poslan u roku od 24h.</td>
             @else
-                <td style="border-left: none; text-align: right; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">{{ number_format($total->value, 2, ',', '.') }}</td>
+                <td style="border-left: none; text-align: right;border-top: none; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">{{ number_format($total->value, 2, ',', '.') }}</td>
             @endif
         </tr>
     @endforeach
