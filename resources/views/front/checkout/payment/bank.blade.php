1747e951a4be2d21b0ec1a2483e8e1fe51334fea
tomashina <tomislav@agmedia.hr>
2023-08-21 09:06:05 +0200

    fix success padding

--- resources/views/front/checkout/payment/bank.blade.php
+++ resources/views/front/checkout/payment/bank.blade.php
@@ -1,6 +1,15 @@
 <form name="pay" class="w-100" action="{{ route('checkout') }}" method="GET">
     <input type="hidden" name="provjera" value="{{ $data['order_id'] }}">
 
+    <div class="form-check form-check-inline">
+        <label class="form-check-label" for="ex-check-4">{!! __('Slažem se sa :terms_of_service', [
+                                                'terms_of_service' => '<a target="_blank" href="'.route('catalog.route.page',['page' => 'opci-uvjeti-kupnje']).'" class="link-fx">'.__('Uvjetima kupovine').'</a>',
+                                                'privacy_policy' => '<a target="_blank" href="'.route('policy.show').'" class="link-fx">'.__('Privacy Policy').'</a>',
+                                        ]) !!}</label>
+        <input class="form-check-input" type="checkbox" name="terms" id="terms" required>
+        <div class="invalid-feedback" id="terms">Morate se složiti sa Uvjetima kupnje.</div>
+    </div>
+
     <div class="d-flex mt-3">
     <div class="w-50 pe-3">
         <a class="btn btn-secondary d-block w-100" href="{{ route('naplata') }}"><i class="ci-arrow-left  me-1"></i><span class="d-none d-sm-inline">Povratak na plaćanje</span><span class="d-inline d-sm-none">Povratak</span></a>
