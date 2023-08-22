67400058bba65a1ba05d7fbb85785fcb9382823b
Filip Jankoski <filip@agmedia.hr>
2023-08-22 14:11:40 +0200

    _fix: Related random order.

--- resources/views/front/catalog/product/index.blade.php
+++ resources/views/front/catalog/product/index.blade.php
@@ -470,7 +470,7 @@
         </div>
         <div class="tns-carousel tns-controls-static tns-controls-outside tns-nav-enabled pt-2">
             <div class="tns-carousel-inner" data-carousel-options="{&quot;items&quot;: 2, &quot;gutter&quot;: 16, &quot;controls&quot;: true, &quot;autoHeight&quot;: true, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:2}, &quot;480&quot;:{&quot;items&quot;:2}, &quot;720&quot;:{&quot;items&quot;:3}, &quot;991&quot;:{&quot;items&quot;:2}, &quot;1140&quot;:{&quot;items&quot;:3}, &quot;1300&quot;:{&quot;items&quot;:4}, &quot;1500&quot;:{&quot;items&quot;:5}}}">
-                @foreach ($cat->products()->get()->unique()->take(10) as $cat_product)
+                @foreach ($cat->products()->get()->random(10) as $cat_product)
                     @if ($cat_product->id  != $prod->id)
                         <div>
                             @include('front.catalog.category.product', ['product' => $cat_product])
