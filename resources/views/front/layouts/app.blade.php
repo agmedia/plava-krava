a2ae3dd33e83339bd45355d9ec346c1179083da0
tomashina <tomislav@agmedia.hr>
2023-09-13 08:43:09 +0200

    gtm

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -85,6 +85,14 @@
     </style>
     @if (config('app.env') == 'production')
         @yield('google_data_layer')
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KWFCSLSV');</script>
+        <!-- End Google Tag Manager -->
         <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-B0BP9R6HH4"></script>
         <script>
@@ -99,6 +107,10 @@
 
 <!-- Body-->
 <body class="bg-secondary">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWFCSLSV"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div id="agapp">
     <div v-cloak>
 
