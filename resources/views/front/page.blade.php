421739bea1ee3ab165c7bc0f124a4ed3efc7b531
tomashina <tomislav@agmedia.hr>
2023-09-15 15:24:27 +0200

    meta decriptio nfixes

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -1,7 +1,7 @@
 @extends('front.layouts.app')
 @if (request()->routeIs(['index']))
-    @section ( 'title', 'Plava Krava | Webshop | Knjige | Knjige na engleskom jeziku' )
-    @section ( 'description', 'Iza simpatičnog imena Plava krava, krije se odlična ekipa koja kupca itekako stavlja na prvo mjesto. Jednostavna kupovina i brza dostava.' )
+    @section ( 'title', 'Plava Krava | Webshop |  Knjige na engleskom jeziku' )
+    @section ( 'description', 'Istražite svijet književnih čuda u našoj online knjižari. Otkrijte bogatu kolekciju knjiga na engleskom jeziku, od klasičnih romana do suvremenih bestselera. Pronađite svoje sljedeće zanimljivo štivo danas!' )
 
 
     @push('meta_tags')
@@ -9,25 +9,25 @@
         <link rel="canonical" href="{{ env('APP_URL')}}" />
         <meta property="og:locale" content="hr_HR" />
         <meta property="og:type" content="product" />
-        <meta property="og:title" content="Plava Krava | Webshop | Knjige | Knjige na engleskom jeziku" />
-        <meta property="og:description" content="Iza simpatičnog imena Plava krava, krije se odlična ekipa koja kupca itekako stavlja na prvo mjesto. Jednostavna kupovina i brza dostava." />
+        <meta property="og:title" content="Plava Krava | Webshop |  Knjige na engleskom jeziku" />
+        <meta property="og:description" content="Istražite svijet književnih čuda u našoj online knjižari. Otkrijte bogatu kolekciju knjiga na engleskom jeziku, od klasičnih romana do suvremenih bestselera. Pronađite svoje sljedeće zanimljivo štivo danas!" />
         <meta property="og:url" content="{{ env('APP_URL')}}"  />
-        <meta property="og:site_name" content="Plava Krava | Webshop | Knjige | Knjige na engleskom jeziku" />
+        <meta property="og:site_name" content="Plava Krava | Webshop |  Knjige na engleskom jeziku" />
         <meta property="og:image" content="{{ asset('media/plavakrava.jpg') }}" />
         <meta property="og:image:secure_url" content="{{ asset('media/plavakrava.jpg') }}" />
         <meta property="og:image:width" content="1920" />
         <meta property="og:image:height" content="720" />
         <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:alt" content="Plava Krava | Webshop | Knjige | Knjige na engleskom jeziku" />
+        <meta property="og:image:alt" content="Plava Krava | Webshop |  Knjige na engleskom jeziku" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Plava Krava | Webshop | Knjige | Knjige na engleskom jeziku" />
-        <meta name="twitter:description" content="Iza simpatičnog imena Plava krava, krije se odlična ekipa koja kupca itekako stavlja na prvo mjesto. Jednostavna kupovina i brza dostava." />
+        <meta name="twitter:title" content="Plava Krava | Webshop |  Knjige na engleskom jeziku" />
+        <meta name="twitter:description" content="Istražite svijet književnih čuda u našoj online knjižari. Otkrijte bogatu kolekciju knjiga na engleskom jeziku, od klasičnih romana do suvremenih bestselera. Pronađite svoje sljedeće zanimljivo štivo danas!" />
         <meta name="twitter:image" content="{{ asset('media/plavakrava.jpg') }}" />
 
     @endpush
 
 @else
-    @section ( 'title', $page->title. ' - Plava Krava' )
+    @section ( 'title', $page->title. ' - Plava Krava |  Knjige na engleskom jeziku' )
     @section ( 'description', $page->meta_description )
 @endif
 
