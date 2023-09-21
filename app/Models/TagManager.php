b384290340581e1d8fb36ee8f42571359084f043
tomashina <tomislav@agmedia.hr>
2023-09-21 11:29:48 +0200

    float fix

--- app/Models/TagManager.php
+++ app/Models/TagManager.php
@@ -73,9 +73,9 @@ class TagManager
         $item = [
             'item_id'        => $product->sku,
             'item_name'      => $product->name,
-            'price'          =>  number_format((float)str_replace(',', '.', $product->main_price), 2),
+            'price'          =>  (float)number_format(str_replace(',', '.', $product->main_price), 2),
             'currency'       => 'EUR',
-            'discount'       =>  number_format((float)$discount, 2),
+            'discount'       =>  (float) number_format($discount, 2),
             'item_category'  => $product->category() ? $product->category()->title : '',
             'item_category2' => $product->subcategory() ? $product->subcategory()->title : '',
             'quantity'       => 1,
