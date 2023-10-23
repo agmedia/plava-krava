b1a7223105856757e17efe839e2b10cefcbff6c6
tomashina <tomislav@agmedia.hr>
2023-10-23 13:23:36 +0200

    kekspay fix

--- resources/views/front/checkout/payment/keks.blade.php
+++ resources/views/front/checkout/payment/keks.blade.php
@@ -1,10 +1,11 @@
-<div class="row text-center hidden-xs">
-    <div class="col-sm-12">
+<div class="d-block w-100">
+<div class="row text-center d-none d-sm-block">
+    <div class="col-12">
         <img src="{{ $data['logo'] }}" style="height: 90px; margin-bottom:30px;" alt="KEKSPAY" />
-        <h3>Plaćanje putem KEKS Pay aplikacije</h3>
+        <h5>Plaćanje putem KEKS Pay aplikacije</h5>
     </div>
-    <div class="col-sm-12">
-        <ul class="instructions">
+    <div class="col-12">
+        <ul style="list-style: none;padding-left: 0;">
             <li>1. Otvori KEKS Pay</li>
             <li>2. Pritisni <img src="{{ asset('media/img/plusikona.svg') }}" style="height: 40px;"/> ikonicu</li>
             <li>3. Pritisni Skeniraj QR kod</li>
@@ -37,19 +38,20 @@
     <input type="hidden" name="store" value="{{ $data['store'] }}">
     <input type="hidden" name="success_url" value="{{ $data['success_url'] }}">
     <input type="hidden" name="fail_url" value="{{ $data['fail_url'] }}">
-    <div class="pull-right kekspay">
-        <input type="submit" value="Potvrdi" class="btn btn-primary visible-xs" />
+    <div class="d-grid gap-2 kekspay">
+        <input type="submit" value="Potvrdi" class="btn btn-primary d-block d-sm-none" />
     </div>
 </form>
-
-<div class="row text-center appblock">
-    <div class="col-sm-12">
-        <h3 style="color:#999">Još nemaš KEKS Pay?</h3>
+<div class="clearfix"></div>
+<div class="row text-center mt-4 appblock">
+    <div class="col-12">
+        <h5 >Još nemaš KEKS Pay?</h5>
         <a href="https://itunes.apple.com/hr/app/keks-pay/id1434843784?l=hr&mt=8">
-            <img src="{{ asset('media/img/appstore.svg') }}" class="getfrom"/>
+            <img style="width:150px" src="{{ asset('media/img/appstore.svg') }}" class="getfrom"/>
         </a>
         <a href="https://play.google.com/store/apps/details?id=agency.sevenofnine.erstewallet.production">
-            <img src="{{ asset('media/img/googleplay.svg') }}" class="getfrom"/>
+            <img style="width:150px" src="{{ asset('media/img/googleplay.svg') }}" class="getfrom"/>
         </a>
     </div>
+</div>
 </div>
