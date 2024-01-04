2acbdd887f7610a2795b431feec2fcf5eb88b221
Filip Jankoski <filip@agmedia.hr>
2024-01-04 09:56:50 +0100

    _fix

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -69,9 +69,8 @@
                                 <h3 class="widget-title text-dark">Načini plaćanja</h3>
                                 <ul class="widget-list">
                                     <li class="widget-list-item"><a href="#" class="widget-list-link"> Kreditnom karticom jednokratno ili na rate</a></li>
-                                    <li class="widget-list-item"><a href="#" class="widget-list-link"> Virmanom / općom uplatnicom / internet bankarstvom</a></li> <li class="widget-list-item"><a href="#" class="widget-list-link">Gotovinom prilikom pouzeća</a></li>
-
-                                    <li class="widget-list-item"><a href="#" class="widget-list-link">Fiksni tečaj konverzije: 1 € = 7,53450 kn.</a></li>
+                                    <li class="widget-list-item"><a href="#" class="widget-list-link"> Virmanom / općom uplatnicom / internet bankarstvom</a></li>
+                                    <li class="widget-list-item"><a href="#" class="widget-list-link">Gotovinom prilikom pouzeća</a></li>
                                 </ul>
                                 <div class="mt-3">
                                     <img src="{{ asset('img/logici/CorvusPAY.svg') }}" loading="lazy" alt="CorvusPay" class="d-inline-block" style="height: 40px; margin-right: 3px;" height="40" width="163px">
