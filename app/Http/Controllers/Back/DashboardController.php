c6b3ed5e8884a82619222a04f5604e111551987f
tomashina <tomislav@agmedia.hr>
2024-09-11 08:56:44 +0200

    dashboard

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -43,7 +43,8 @@ class DashboardController extends Controller
         $data['proccess']   = Order::whereIn('order_status_id', [1, 2, 3])->count();
         $data['finished']   = Order::whereIn('order_status_id', [4, 5, 6, 7])->count();
         $data['this_month'] = Order::whereMonth('created_at', '=', Carbon::now()->month)->count();
-        $data['this_month_total'] = Order::whereMonth('created_at', '=', Carbon::now()->month)->whereIn('order_status_id', [4, 1, 2, 3])->sum('total');
+        $data['this_month_total'] = Order::whereMonth('created_at', '=', Carbon::now()->month)->whereYear('created_at', '=', Carbon::now()->year)->whereIn('order_status_id', [4, 1, 2, 3])->sum('total');
+        $data['this_month_total'] =  number_format($data['this_month_total'], 2,'.','');
 
 
         $data['users']   = UserDetail::whereIn('role', ['customer'])->count();
